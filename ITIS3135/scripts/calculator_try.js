6aab71c6fc7e7e6895c7ea8f4ce6c32693aa6f5d
ChrisDeMarchi <chrisdemarchi779@gmail.com>
2024-03-18 19:29:22 -0400

    Update calculator_try.js

--- ITIS3135/scripts/calculator_try.js
+++ ITIS3135/scripts/calculator_try.js
@@ -1,22 +1,23 @@
 window.onload = function () {
-    document.getElementById("button7").onclick = function() { appendNum(7); };
-    document.getElementById("button8").onclick = function() { appendNum(8); };
-    document.getElementById("button9").onclick = function() { appendNum(9); };
-    document.getElementById("buttonClear").onclick = clearDisplay;
-    document.getElementById("button4").onclick = function() { appendNum(4); };
-    document.getElementById("button5").onclick = function() { appendNum(5); };
-    document.getElementById("button6").onclick = function() { appendNum(6); };
-    document.getElementById("buttonAdd").onclick = function() { operate('+'); };
-    document.getElementById("button1").onclick = function() { appendNum(1); };
-    document.getElementById("button2").onclick = function() { appendNum(2); };
-    document.getElementById("button3").onclick = function() { appendNum(3); };
-    document.getElementById("buttonSubtract").onclick = function() { operate('-'); };
-    document.getElementById("button0").onclick = function() { appendNum(0); };
-    document.getElementById("buttonDecimal").onclick = appendDecimal;
-    document.getElementById("buttonCalculate").onclick = calculate;
-    document.getElementById("buttonMultiply").onclick = function() { operate('*'); };
-    document.getElementById("buttonDivide").onclick = function() { operate('/'); };
+    document.getElementById("button7").addEventListener("click", function() { appendNum(7); });
+    document.getElementById("button8").addEventListener("click", function() { appendNum(8); });
+    document.getElementById("button9").addEventListener("click", function() { appendNum(9); });
+    document.getElementById("buttonClear").addEventListener("click", clearDisplay);
+    document.getElementById("button4").addEventListener("click", function() { appendNum(4); });
+    document.getElementById("button5").addEventListener("click", function() { appendNum(5); });
+    document.getElementById("button6").addEventListener("click", function() { appendNum(6); });
+    document.getElementById("buttonAdd").addEventListener("click", function() { operate('+'); });
+    document.getElementById("button1").addEventListener("click", function() { appendNum(1); });
+    document.getElementById("button2").addEventListener("click", function() { appendNum(2); });
+    document.getElementById("button3").addEventListener("click", function() { appendNum(3); });
+    document.getElementById("buttonSubtract").addEventListener("click", function() { operate('-'); });
+    document.getElementById("button0").addEventListener("click", function() { appendNum(0); });
+    document.getElementById("buttonDecimal").addEventListener("click", appendDecimal);
+    document.getElementById("buttonCalculate").addEventListener("click", calculate);
+    document.getElementById("buttonMultiply").addEventListener("click", function() { operate('*'); });
+    document.getElementById("buttonDivide").addEventListener("click", function() { operate('/'); });
 };
+
 
 let displayValue = '0';
 let operator = null;
