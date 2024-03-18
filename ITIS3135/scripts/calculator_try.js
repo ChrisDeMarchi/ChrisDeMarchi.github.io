65f07bd661e804e09ec38c9524a368146cce74bd
ChrisDeMarchi <chrisdemarchi779@gmail.com>
2024-03-18 19:25:15 -0400

    fixed the calculator to remove imbedded js

--- ITIS3135/scripts/calculator_try.js
+++ ITIS3135/scripts/calculator_try.js
@@ -1,3 +1,23 @@
+window.onload = function () {
+    document.getElementById("button7").onclick = function() { appendNum(7); };
+    document.getElementById("button8").onclick = function() { appendNum(8); };
+    document.getElementById("button9").onclick = function() { appendNum(9); };
+    document.getElementById("buttonClear").onclick = clearDisplay;
+    document.getElementById("button4").onclick = function() { appendNum(4); };
+    document.getElementById("button5").onclick = function() { appendNum(5); };
+    document.getElementById("button6").onclick = function() { appendNum(6); };
+    document.getElementById("buttonAdd").onclick = function() { operate('+'); };
+    document.getElementById("button1").onclick = function() { appendNum(1); };
+    document.getElementById("button2").onclick = function() { appendNum(2); };
+    document.getElementById("button3").onclick = function() { appendNum(3); };
+    document.getElementById("buttonSubtract").onclick = function() { operate('-'); };
+    document.getElementById("button0").onclick = function() { appendNum(0); };
+    document.getElementById("buttonDecimal").onclick = appendDecimal;
+    document.getElementById("buttonCalculate").onclick = calculate;
+    document.getElementById("buttonMultiply").onclick = function() { operate('*'); };
+    document.getElementById("buttonDivide").onclick = function() { operate('/'); };
+};
+
 let displayValue = '0';
 let operator = null;
 let firstOperand = null;
